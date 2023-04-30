34d7f599f6725a91af6bef2a0c5202b9cda49aaa
ashu-dev-ops <computerlogic25@gmail.com>
2023-04-30 16:41:28 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FoodFormComponent } from './food-form/food-form.component';
+import { FoodItemComponent } from './food-item/food-item.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodFormComponent,
+    FoodItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/food-form/food-form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-food-form',
+  templateUrl: './food-form.component.html',
+  styleUrls: ['./food-form.component.scss'],
+})
+export class FoodFormComponent {
+  foodForm = new FormGroup({
+    food: new FormControl(''),
+    // password: new FormControl(''),
+  });
+  foods: { food: string }[] = [];
+  onSubmit() {
+    this.foods.push({ food: this.foodForm.value.food as string });
+    this.foodForm.get('food')?.reset();
+  }
+  onDeleteFood(event: any) {
+    let temp: { food: string }[] = [];
+    temp = this.foods.filter((i) => i.food !== event);
+    this.foods = temp;
+  }
+}
